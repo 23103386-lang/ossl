34ade48551dc2a2e0a675f9d5a83e8340d438786
23103386 <23103386@pdc.jiit>
2025-07-30 10:37:09 +0530

    Add: lab 1 q2

---
+++ lab1_calc_avg.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> arr;
+    cout<<"size = "<<end;
+    int size;
+    cin>>size;
+    int sum = 0;
+    cout<<"arr :"<<endl;
+    for(int i = 0; i<size; i++){
+            int x;
+            cin>>x;
+            sum+=x;
+        arr.push_back(x);
+    }
+    avg = sum/size;
+
+    cout<<avg<<endl;
+}
